7c9f770e7d123eb8b2c4ed6e2a6c5c2ad685b562
Aluno <aluno@naverecife.local>
2018-08-20 14:37:57 -0300

    InicioVida

---
+++ Assets/Scripts/PlayerLife.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLife : MonoBehaviour {
+
+	public int currentLife;
+	public int maxLife;
+
+	public Sprite[] lifeSprite;
+	
+	// Use this for initialization
+	void Start () {
+		currentLife = maxLife;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
