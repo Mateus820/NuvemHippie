4b470ce23be8d7c8fd4718c8eebbe248b90a8b38
Aluno <aluno@naverecife.local>
2018-08-23 10:07:12 -0300

    Vida etc

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speedQuantity = 3f;
+    private Rigidbody2D bori;
+    public bool grounded;
+    public Vector2 jumpVector;
+    public Transform grounder;
+    public float radius;
+    public LayerMask ground;
+
+
+    void Start () {
+        bori = GetComponent<Rigidbody2D>();
+    }
+    
+    
+    void Update () {
+        
+        if(Input.GetKey(KeyCode.A))
+        {
+            bori.velocity = new Vector2(-speedQuantity, bori.velocity.y);
+            transform.localScale = new Vector3 (-1,1,1);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            bori.velocity = new Vector2(speedQuantity, bori.velocity.y);
+            transform.localScale = new Vector3 (1,1,1);
+        }
+        else
+        {
+            bori.velocity = new Vector2(0, bori.velocity.y);
+        }
+
+
+        grounded = Physics2D.OverlapCircle(grounder.transform.position, radius, ground);
+
+        if(Input.GetKey(KeyCode.W) && grounded == true)
+        {
+            bori.AddForce(jumpVector, ForceMode2D.Force);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,21 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerLife : MonoBehaviour {
 
 	public int currentLife;
 	public int maxLife;
+    public Image life_Hud;
 
 	public Sprite[] lifeSprite;
 	
-	// Use this for initialization
 	void Start () {
 		currentLife = maxLife;
 	}
-	
-	// Update is called once per frame
 	void Update () {
-		
+        switch (currentLife)
+        {
+            case 0:
+                life_Hud.sprite = lifeSprite[0];
+                break;
+            case 1:
+                life_Hud.sprite = lifeSprite[1];
+                break;
+            case 2:
+                life_Hud.sprite = lifeSprite[2];
+                break;
+            case 3:
+                life_Hud.sprite = lifeSprite[3];
+                break;
+            default:
+                life_Hud.sprite = lifeSprite[0];
+                break;
+        }
 	}
 }
