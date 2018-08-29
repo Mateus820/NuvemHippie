b4dc9492c89c08f7f94865072504da3117f32ae2
Aluno <aluno@naverecife.local>
2018-08-29 13:13:56 -0300

    GameData

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,7 +8,7 @@ public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
 
-	public PlayerLife playerlife;
+	public PlayerLife playerLife;
 	private const string FILE_PATH = "saveGameData.dat";
 
 	void Awake() {
@@ -20,36 +20,36 @@ public class GameManager : MonoBehaviour {
 			Destroy(gameObject);
 		}
 	}
-	void Start () {
-		
-	}
-	void Update () {
-	
+	void Start() {
+		Load();
 	}
 	public void SaveGame()
 	{
 		BinaryFormatter bf = new BinaryFormatter();
 		FileStream file = File.Create(Path.Combine(Application.streamingAssetsPath, FILE_PATH));
 
-		saveGameData save = new saveGameData();
-		save.lifes = playerlife.currentLife;
+		SaveGameData save = new SaveGameData();
+		save.lifes = playerLife.CurrentLife;
 		save.points = 0;
 
 		bf.Serialize(file, save);
 		file.Close();
 
 	}
-		public void Load()
-		{
-			if (File.Exists())
+	public void Load()
+	{
+		if (File.Exists(Path.Combine(Application.streamingAssetsPath, FILE_PATH))){
+			BinaryFormatter bf = new BinaryFormatter();
+			FileStream file = File.Open(Path.Combine(Application.streamingAssetsPath, FILE_PATH), FileMode.Open);
+
+			SaveGameData save = (SaveGameData) bf.Deserialize(file);
+
+			file.Close();
+
+			playerLife.CurrentLife = save.lifes;
 		}
-
-}
-
-[Serizable]
-class saveGameData
-{
-	public int lifes;
-	public int points;
-
+	}
+	void OnApplicationQuit() {
+		SaveGame();
+	}
 }
