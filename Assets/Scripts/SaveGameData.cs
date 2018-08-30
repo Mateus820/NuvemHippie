3de8d838daa7136fa16712208471cee48c8cbb0a
Aluno <aluno@naverecife.local>
2018-08-30 10:03:57 -0300

    Salvamento

--- Assets/Scripts/SaveGameData.cs
+++ Assets/Scripts/SaveGameData.cs
@@ -1,11 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 [Serializable]
 class SaveGameData
 {
 	public int lifes;
 	public int points;
-
 }
--- Assets/Scripts/TESTPlayer.cs
+++ Assets/Scripts/TESTPlayer.cs
@@ -2,24 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class TESTPlayer : MonoBehaviour {
 
-public float playerSpeed;  //allows us to be able to change speed in Unity
-public Vector2 jumpHeight;
+    public float playerSpeed;  //allows us to be able to change speed in Unity
+    public Vector2 jumpHeight;
 
-// Use this for initialization
-void Start () {
-
-}
-// Update is called once per frame
-void Update ()
-{
-    transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f);  //makes player run
-
-    if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+    void Update ()
     {
-        GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
-}
-}
+        transform.Translate(playerSpeed * Time.deltaTime, 0f, 0f);  //makes player run
+
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))  //makes player jump
+        {                
+            GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+        }
+    }
 }
