59c265b0740e034a27c16f7eb0455fac72899331
Aluno <aluno@naverecife.local>
2018-08-30 09:30:56 -0300

    juni2

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,59 +4,6 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
 
-<<<<<<< HEAD
-public class GameManager : MonoBehaviour
-{
-
-    public static GameManager instance;
-
-    public PlayerLife playerlife;
-    private const string FILE_PATH = "saveGameData.dat";
-
-    void Awake()
-    {
-        if (instance == null)
-        {
-            instance = this;
-            DontDestroyOnLoad(gameObject);
-        }
-        else if (instance != this)
-        {
-            Destroy(gameObject);
-        }
-    }
-    void Start()
-    {
-
-    }
-    void Update()
-    {
-
-    }
-    public void SaveGame()
-    {
-        BinaryFormatter bf = new BinaryFormatter();
-        FileStream file = File.Create(Path.Combine(Application.streamingAssetsPath, FILE_PATH));
-
-        saveGameData save = new saveGameData();
-        save.lifes = playerlife.currentLife;
-        save.points = 0;
-
-        bf.Serialize(file, save);
-        file.Close();
-
-    }
-    public void Load()
-    {
-        //if (File.Exists())
-    }
-
-}
-class saveGameData
-{
-	public int lifes;
-	public int points;
-=======
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
@@ -98,7 +45,6 @@ public class GameManager : MonoBehaviour {
 			SaveGameData save = (SaveGameData) bf.Deserialize(file);
 
 			file.Close();
->>>>>>> upstream/master
 
 			playerLife.CurrentLife = save.lifes;
 		}
