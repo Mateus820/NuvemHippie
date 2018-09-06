099b6d6e84f9d231cc32f5ca0974a87631373ce5
Mateus820 <37410480+Mateus820@users.noreply.github.com>
2018-09-06 10:01:49 -0300

    06/09_0.3

---
+++ Assets/Scripts/Inventory/ConsumableItem.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ConsumableItem", menuName = "Inventory/ConsumableItem", order = 0)]
+public class ConsumableItem : ScriptableObject {
+	public string name;
+	public int id;
+	public Sprite sprite;
+	public string description;
+}
---
+++ Assets/Scripts/Inventory/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Inventory/Weapon", order = 0)]
+public class Weapon : ScriptableObject {
+
+	public string name;
+	public int id;
+	public Sprite sprite;
+	public string description;
+}
