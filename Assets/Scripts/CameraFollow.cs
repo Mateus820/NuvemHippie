9f128951efe9094b460471abf2dd5bbf32a26f32
Mateus820 <37410480+Mateus820@users.noreply.github.com>
2018-09-06 10:12:27 -0300

    06/09_0.3

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float speed = 0.15f;
+	[SerializeField] private Transform target;
+
+	public bool maxMin;
+	public float xMin;
+	public float yMin;
+	public float xMax;
+	public float yMax;
+
+	void Update () {
+
+		if (target) {
+			transform.position = Vector3.Lerp (transform.position, target.position, speed) + new Vector3(0,0,target.position.z);
+			if (maxMin) {
+				transform.position = new Vector3 (Mathf.Clamp (target.position.x, xMin, xMax), Mathf.Clamp (target.position.y, yMin, yMax), 2*target.position.z);
+			}
+		}
+
+	}
+}
