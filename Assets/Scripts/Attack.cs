f0e3c31cff7e1ffe80a08e91bf83c39d780679c7
trash <aluno@naverecife.local>
2018-09-17 14:21:33 -0300

    ataque completo

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attack : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject attack;
+	private IEnumerator dlay;
+	[SerializeField]
+	public bool attacking;
+
+	void Start () {
+		attacking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.C) && !attacking){
+			Wait();
+		}
+	}
+
+	void Wait(){
+		dlay = AttackDlay(0.5f);
+		StartCoroutine(dlay);
+	}
+
+	IEnumerator AttackDlay (float time){
+		attacking = true;
+		attack.SetActive (true);
+        yield return new WaitForSeconds(time);
+		attack.SetActive (false);
+		yield return new WaitForSeconds(0.5f);
+		attacking = false;
+	}
+}
