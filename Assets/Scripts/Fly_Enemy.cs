b097e59a5ee1c3ebb0f654c60116dcd8888a61e1
trash <aluno@naverecife.local>
2018-09-20 10:03:29 -0300

    juncao_0.3

---
+++ Assets/Scripts/Fly_Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fly_Enemy : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public Rigidbody2D targetRb;
+	public float speed;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.position = Vector3.MoveTowards(rb.position, targetRb.position, Mathf.Abs(speed) * Time.deltaTime);
+	}
+}
